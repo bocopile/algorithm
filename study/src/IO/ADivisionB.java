5850f90851888c4096f9037afa7d13a3cd2a7132
gjrjr4545 <gjrjr4545@gmail.com>
2020-11-17 23:01:08 +0900

    11월 17일 - 입출력 관련 사칙연산

---
+++ study/src/IO/ADivisionB.java
@@ -0,0 +1,16 @@
+package IO;
+
+import java.util.Scanner;
+
+public class ADivisionB {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+
+        System.out.println(a/b);
+    }
+}
